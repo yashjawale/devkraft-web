df212a9b944127a025bc4712ccc19f015e785afd
yashjawale <yash25.j@gmail.com>
2022-04-17 18:38:19 +0530

    Finishing Touches

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <HeadedParagraph title="Who are we?">
         We are a team of passionate students constantly finding ways to make the
         lives of students at{" "}
-        <IconLink to="https://engg.dypvp.edu.in">
+        <IconLink to="https://engg.dypvp.edu.in" external>
           Dr. D. Y. Patil Institute of Technology, Pimpri
         </IconLink>
         more seamless and enjoyable. We organise various events, workshops and
@@ -20,13 +20,16 @@ const Home = () => {
         excepturi veritatis id deserunt? Dolorem, quisquam sed voluptas illum
         officiis vel cumque quos ducimus esse labore sint?
       </HeadedParagraph>
-      <HeadedParagraph title="Who are we?">
+      <HeadedParagraph title="Join our Team!">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
         placeat voluptates accusamus minima obcaecati itaque, dolores temporibus
         est nam sint ab impedit, sit quam perferendis eos rerum nisi doloribus
         amet, laborum corporis culpa excepturi veritatis id deserunt? Dolorem,
         quisquam sed voluptas illum officiis vel cumque quos ducimus esse labore
         sint?
+        <a href="#" className="button">
+          Join Team
+        </a>
       </HeadedParagraph>
     </>
   );
